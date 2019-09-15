3f355cdec9b2299c7d24416d0095898a5acf1bfc
cao yin <caoyin2011@163.com>
2019-09-15 08:47:36 +0800

    add base wsgi on it

--- helloapp/helloapp/settings.py
+++ helloapp/helloapp/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '5x!x$7=--&ob$hjk5h*@t871w=cko^u^bzfo&gsgs2r5d^xguf'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*",]
 
 
 # Application definition
